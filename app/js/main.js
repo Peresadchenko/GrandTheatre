ea25118836991d95303419db84433449eedf5c8c
Peresadchenko <1pers1@bk.ru>
2022-02-01 03:23:16 +0300

    add header and about

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,19 @@
-$(function() {
-  
+$(function () {
+
+    $('.header__slider-inner').slick({
+        arrows: false,
+        fade: true,
+        autoplay: false,
+        autoplaySpeed: 2000
+    });
+
+
+    $('.header__slider-btnPrev').click(function (event) {
+        $('.header__slider-inner').slick("slickPrev");
+    });
+    $('.header__slider-btnNext').click(function (event) {
+        $('.header__slider-inner').slick("slickNext");
+    });
+
 });
+
