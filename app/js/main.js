23934bea00eaaf5324dedca1d45c2880c6e338ee
Peresadchenko <1pers1@bk.ru>
2022-02-01 17:02:09 +0300

    add critics partners contracts footer

--- app/js/main.js
+++ app/js/main.js
@@ -7,6 +7,17 @@ $(function () {
         autoplaySpeed: 2000
     });
 
+    $('.productions__slider-inner').slick({
+        arrows: false,
+        adaptiveHeight: true,
+        slidesToShow: 4,
+        slidesToScroll: 3,
+        speed: 1000,
+        easing: 'ease',
+        autoplay: true,
+        autoplaySpeed: 2000
+    });
+
 
     $('.header__slider-btnPrev').click(function (event) {
         $('.header__slider-inner').slick("slickPrev");
