34cf17813d8d4e8e36195d1d36eb9a5687c8c433
Peresadchenko <1pers1@bk.ru>
2022-02-05 23:29:48 +0300

    Updates

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 $(function () {
 
+    $('.menu__btn').on('click', function () {
+        $('.menu__list').toggleClass('menu__list--active');
+        $('.menu__list-social').toggleClass('menu__list-social--active');
+        $(".menu").toggleClass("menu--open");
+      });
+
     $('.header__slider-inner').slick({
         arrows: false,
         fade: true,
@@ -8,10 +14,24 @@ $(function () {
     });
 
     $('.productions__slider-inner').slick({
+        responsive: [
+            {
+                breakpoint: 993,
+                settings: {
+                    slidesToShow: 1.5,
+                    slidesToScroll: 1.5
+                }
+            },
+        ],
         arrows: false,
         adaptiveHeight: true,
+        cssEase: 'linear',
         slidesToShow: 4,
-        slidesToScroll: 3,
+        speed: 800,
+        slidesToScroll: 4,
+        initialSlide: 0,
+        infinite: false,
+        variableWidth: true,
         speed: 1000,
         easing: 'ease',
         // autoplay: true,
