678db84de67e68d02906a3215e1c0d795c73c0bf
Peresadchenko <1pers1@bk.ru>
2022-02-09 20:42:18 +0300

    Updates

--- js/main.js
+++ js/main.js
@@ -2,9 +2,9 @@ $(function () {
 
     $('.menu__btn').on('click', function () {
         $('.menu__list').toggleClass('menu__list--active');
-        $('.menu__list-social').toggleClass('menu__list-social--active');
         $(".menu").toggleClass("menu--open");
-      });
+    });
+
 
     $('.header__slider-inner').slick({
         arrows: false,
@@ -19,7 +19,7 @@ $(function () {
                 breakpoint: 993,
                 settings: {
                     slidesToShow: 1.5,
-                    slidesToScroll: 1.5
+                    slidesToScroll: 1
                 }
             },
         ],
@@ -28,10 +28,48 @@ $(function () {
         cssEase: 'linear',
         slidesToShow: 4,
         speed: 800,
-        slidesToScroll: 4,
+        slidesToScroll: 1,
         initialSlide: 0,
         infinite: false,
         variableWidth: true,
+        speed: 1000,
+        easing: 'ease',
+        // autoplay: true,
+        autoplaySpeed: 2000
+    });
+
+    $('.partners__slider-inner').slick({
+        responsive: [
+            {
+                breakpoint: 1300,
+                settings: {
+                    slidesToShow: 2.5,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            
+            {
+                breakpoint: 550,
+                settings: {
+                    slidesToShow: 1.2,
+                    slidesToScroll: 1
+                }
+            },
+        ],
+        arrows: false,
+        cssEase: 'linear',
+        slidesToShow: 4,
+        speed: 800,
+        slidesToScroll: 2,
+        initialSlide: 0,
+        infinite: false,
         speed: 1000,
         easing: 'ease',
         // autoplay: true,
